30e8b6d0f953e8e06e2056efb1e3d1bcf90e9b6d
Samridh Tuladhar <t.samridh.86@gmail.com>
2016-12-15 09:28:13 +0545

    completed key generation

--- keyGenerator.py
+++ keyGenerator.py
@@ -12,6 +12,7 @@
 
 # imports
 from random import randrange
+from math import floor
 
 # returns 0 if the number is prime , returns 1 otherwise
 def checkPrime ( num ):
@@ -38,6 +39,26 @@ def gcd (a, b):
 	else :
 		return gcd(b,a%b)	
 
+#extended euclid's algorithm, returns 3 values, gcd , x & y
+def extendedEuclid(a , b):
+	if b == 0 :
+		return (a,1,0)
+	else:
+		d1 , x1, y1 = extendedEuclid(b, a%b)
+		d,x,y = d1, y1, x1 - floor(a/b) * y1
+		return d,x,y	
+
+#this function ins used to calculate modulo using extended euclid's algorithm
+def getModuloInverse ( a, b ):
+	x, y, z = extendedEuclid(a,b)
+	# only y is needed, discard the rest
+	# if it is positive, send back else subtract from phi
+	if y > 0 :
+		return y
+	else :
+		#y is already -ve so minus minus would be plus
+		return b + y	
+
 
 # main code execution begins here
 # get 2 prime numbers & make sure they are prime
@@ -55,12 +76,19 @@ n = p * q
 phi = (p-1) * (q-1)
 
 #get a random number e such that 1<e<phi, but gcd(e,phi) == 1
-e = randrange(1,phi)
+e = randrange(2,phi)
 while gcd (e, phi) != 1 :
-	e = randrange(1,phi)
-print (e)
+	e = randrange(1,phi)	
+
+#compute d = e inv mod phi
+d = getModuloInverse(e,phi)
 
 # compute suitable blocksize
 blockSize = getBlockSize(n)
 if blockSize < 1 :
 	exit()
+
+#completed, display output
+print ("public key : ",e," , ",n)
+print ("private key : ",d," , ",n)
+print ("preferable blockSize : ",blockSize)
